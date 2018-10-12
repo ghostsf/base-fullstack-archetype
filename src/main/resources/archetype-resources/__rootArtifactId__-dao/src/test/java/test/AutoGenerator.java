ae789d9e6ff9ca21398e252ce0c02de4361e9646
ghostsf <ghost_sf@163.com>
2018-10-12 15:53:03 +0800

    - remove useless - add mybatis auto generator - fix log path - fix data-sources.properties

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-dao/src/test/java/test/AutoGenerator.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package ${package}.test;
+
+import com.blackuio.base.sqlgenerator.Generator;
+import java.io.IOException;
+
+/**
+ *
+ * @author  ghostsf
+ * @datetime 2018/10/12 下午3:45
+ * @version
+ * @see
+ * @since
+ */
+public class AutoGenerator {
+
+	public static void main(String[] args) throws IOException, Exception {
+		Generator.execute();
+	}
+
+}
